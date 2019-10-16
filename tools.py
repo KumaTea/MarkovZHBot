105d66c85b05125fc1c7424c5d764157f9670708
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-10-16 22:21:05 +0800

    0.3.2.2

--- tools.py
+++ tools.py
@@ -8,5 +8,6 @@ def get_chat_admin(chat_id):
     except FileNotFoundError:
         admin = bot.query(chat_id).group_admin()
         with open(f'admin{chat_id}.txt', 'w') as f:
-            f.write('\n'.join(str(admin)))
+            for i in admin:
+                f.write(str(i) + '\n')
     return admin
